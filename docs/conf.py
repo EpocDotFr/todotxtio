9925ac60b1c68dbe544c337d622b959eac4afba9
EpocDotFr <contact@epoc.fr>
2017-01-18 09:15:31 +0100

    Add a TOC to the docs

--- docs/conf.py
+++ docs/conf.py
@@ -181,7 +181,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #
-html_sidebars = { '**': [] }
+html_sidebars = { '**': ['localtoc.html'] }
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
